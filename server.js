5bb924bc2def6c743de4016b78b9ea8b1d46f70c
Taiyr <Taiyrkul@gmail.com>
2020-05-29 08:39:11 +0600

    s

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const jsonServer = require('json-server');
+const bodyParser = require('body-parser');
+
 const server = jsonServer.create();
 const router = jsonServer.router('files.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
 server.use(middlewares);
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({extended: true}));
+server.use(bodyParser.json());
+
+
 server.use(router);
 
 server.listen(port);
